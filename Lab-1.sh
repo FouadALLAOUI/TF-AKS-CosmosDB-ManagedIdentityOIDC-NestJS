9279c094257ace2fa34fd04d683224922cc02d6f
FouadAllaoui <fouadallaoui70@gmail.com>
2025-01-26 22:49:29 +0100

    feat: Start the Lab1

---
+++ Lab-1.sh
@@ -0,0 +1,54 @@
+cd ./Terraforming
+
+terraform init 
+terraform fmt
+terraform validate
+terraform plan
+terraform apply -auto-approve
+
+
+cd ../K8S/Demo0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Destroy the resources created by terraform
+#terraform destroy -auto-approve
+
+
+
+
